199f2094d4847452b31aeb9de03c64539f0e336e
konkokonos <73117962+konkokonos@users.noreply.github.com>
2023-03-24 18:23:25 +0200

    Update SecurityConfig.java

--- src/main/java/com/example/mefit/config/SecurityConfig.java
+++ src/main/java/com/example/mefit/config/SecurityConfig.java
@@ -23,7 +23,7 @@ public class SecurityConfig {
                 .csrf().disable()
                 // Enable security for http requests
                 .authorizeHttpRequests(authorize -> authorize
-                    .requestMatchers(HttpMethod.GET, "/api/v1/exercises").hasRole("Contributor")
+                    .requestMatchers(HttpMethod.GET, "/api/v1/exercises").hasAnyRole("Contributor")
                     //.requestMatchers(HttpMethod.GET, "/api/v1/workouts").hasAuthority("Contributor")
                     //.requestMatchers(HttpMethod.GET, "/api/v1/goals").hasRole("Administrator")
                     //.requestMatchers(HttpMethod.GET, "/api/v1/profiles").hasAuthority("Administrator")
