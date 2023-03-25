c572ced8157af7f986397abbd27d12545fdc5f3e
konkokonos <73117962+konkokonos@users.noreply.github.com>
2023-03-25 17:47:55 +0200

    Update SecurityConfig.java

--- src/main/java/com/example/mefit/config/SecurityConfig.java
+++ src/main/java/com/example/mefit/config/SecurityConfig.java
@@ -6,10 +6,16 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
-import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.core.convert.converter.Converter;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @EnableWebSecurity
 @Configuration
@@ -34,22 +40,24 @@ public class SecurityConfig {
 
         http.oauth2ResourceServer()
            .jwt()
-           .jwtAuthenticationConverter(jwtRoleAuthenticationConverter());
+           .jwtAuthenticationConverter(jwtAuthenticationConverterForKeycloak());
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }
 
     @Bean
-    public JwtAuthenticationConverter jwtRoleAuthenticationConverter() {
-        Jwt jwt;
-        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
-        // Use roles claim as authorities
-        grantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
-        // Add the ROLE_ prefix - for hasRole
-        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+    public JwtAuthenticationConverter jwtAuthenticationConverterForKeycloak() {
+        Converter<Jwt, Collection<GrantedAuthority>> jwtGrantedAuthoritiesConverter = jwt -> {
+            Map<String, Collection<String>> realmAccess = jwt.getClaim("realm_access");
+            Collection<String> roles = realmAccess.get("roles");
+            return roles.stream()
+                    .map(role -> new SimpleGrantedAuthority("ROLE_" + role))
+                    .collect(Collectors.toList());
+        };
 
-        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
-        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+
         return jwtAuthenticationConverter;
     }
 }
