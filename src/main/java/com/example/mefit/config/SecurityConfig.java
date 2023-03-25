c2abc6e0a45c945a24ef488ef9fe8d2e34430abd
konkokonos <73117962+konkokonos@users.noreply.github.com>
2023-03-25 15:26:39 +0200

    Update SecurityConfig.java

--- src/main/java/com/example/mefit/config/SecurityConfig.java
+++ src/main/java/com/example/mefit/config/SecurityConfig.java
@@ -22,17 +22,17 @@ public class SecurityConfig {
             //.csrf().disable()
             // Enable security for http requests
             .authorizeHttpRequests()
-                .requestMatchers(HttpMethod.GET, "/api/v1/exercises").hasRole("Contributor")
+                .requestMatchers(HttpMethod.GET, "/api/v1/exercises").hasAuthority("Contributor")
                 //.requestMatchers(HttpMethod.GET, "/api/v1/workouts").hasAuthority("Contributor")
                 //.requestMatchers(HttpMethod.GET, "/api/v1/goals").hasRole("Administrator")
                 //.requestMatchers(HttpMethod.GET, "/api/v1/profiles").hasAuthority("Administrator")
                 // All remaining paths require authentication
                 .anyRequest().authenticated();
                 
-        http.oauth2ResourceServer()
-            .jwt()
-            .jwtAuthenticationConverter(jwtRoleAuthenticationConverter());
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        //http.oauth2ResourceServer()
+        //    .jwt()
+        //    .jwtAuthenticationConverter(jwtRoleAuthenticationConverter());
+        //http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }
 
